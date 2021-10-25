74be5997af9724d81c5916e28f47ed9151327cf6
HabibIbrahimov <habibyi@code.edu.az>
2021-10-26 01:18:17 +0400

    first commit

---
+++ CustomList/DataStore.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomList
+{
+    class MyList<T>
+    {
+        private T[] arr;
+
+        public MyList()
+        {
+            arr = new T[0];
+        }
+
+        public void Add(T num)
+        {
+            Array.Resize(ref arr, arr.Length + 1);
+            arr[arr.Length - 1] = num;
+        }
+
+        //public void AddRange(IEnumerable<T> collection)
+        //{
+
+        //}
+
+        public void GetAll()
+        {
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        //Indexer
+
+        //    private T[] store;
+        //    public MyList()
+        //    {
+        //        store = new T[5];
+        //    }
+
+        //    public MyList(int length)
+        //    {
+        //        store = new T[length];
+        //    }
+
+        //    public T this[int index]
+        //    {
+        //        get
+        //        {
+        //            try
+        //            {
+        //                return store[index];
+        //            }
+        //            catch (Exception)
+        //            {
+
+        //                return store[0];
+        //            }
+        //        }
+        //        set
+        //        {
+        //            try
+        //            {
+        //                store[index] = value;
+        //            }
+        //            catch (Exception)
+        //            {
+
+        //                Console.WriteLine("Out of range");
+        //            }
+        //        }
+        //    }
+
+        //    public int Length
+        //    {
+        //        get
+        //        {
+        //            return store.Length;
+        //        }
+        //    }
+    }
+
+
+}   
---
+++ CustomList/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //MyList<int> grades = new MyList<int>();
+            //grades[0] = 100;
+            //grades[1] = 25;
+            //grades[2] = 34;
+            //grades[3] = 42;
+            //grades[4] = 12;
+
+            //for (int i = 0; i < grades.Length; i++)
+            //    Console.WriteLine(grades[i]);
+
+
+
+            MyList<int> intList = new MyList<int>();
+            intList.Add(10);
+            intList.Add(20);
+            
+
+            //int[] n = new int[] { 1, 2, 3 };
+
+
+
+            //foreach (var item in n)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+        }
+    }
+}
